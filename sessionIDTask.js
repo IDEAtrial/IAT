3587328f1ca587561de5c63d27ab4437b226f9a2
IDEAtrial <155665684+IDEAtrial@users.noreply.github.com>
2024-01-12 14:39:49 -0600

    Update sessionIDTask.js

--- sessionIDTask.js
+++ sessionIDTask.js
@@ -1,7 +1,53 @@
 define(['questAPI'], function(Quest){
-
     let API = new Quest();
+    let isTouch = API.getGlobal().$isTouch;
+	
+    /**
+	* Page prototype
+	*/
+    API.addPagesSet('basicPage',{
+        noSubmit:false, //Change to true if you don't want to show the submit button.
+        header: 'SessionId',
+        decline: true,
+        declineText: isTouch ? 'Decline' : 'Decline to Answer', 
+        autoFocus:true, 
+        progressBar:  'Page <%= pagesMeta.number %> out of 3'
+    });
+	
+    /**
+	* Question prototypes
+	*/
+    API.addQuestionsSet('basicQ',{
+        decline: 'true',
+        required : true, 		
+        errorMsg: {
+            required: isTouch 
+                ? 'Please select an answer, or click \'Decline\'' 
+                : 'Please select an answer, or click \'Decline to Answer\''
+        },
+        autoSubmit:'true',
+        numericValues:'true',
+        help: '<%= pagesMeta.number < 3 %>',
+        helpText: 'Tip: For quick response, click to select your answer, and then click again to submit.'
+    });
 
+    API.addQuestionsSet('basicSelect',{
+        inherit :'basicQ',
+        type: 'selectOne'
+    });
+	
+    API.addQuestionsSet('basicDropdown',{
+        inherit :'basicQ',
+        type : 'dropdown',
+        autoSubmit:false
+    });
+
+
+	
+    /**
+	*Specific questions
+	*/	
+     
     API.addQuestionsSet('sessionIDTask', [{
         type: 'text',
         name: 'sessionID',
@@ -9,6 +55,18 @@ define(['questAPI'], function(Quest){
         required: true
     }]);
 
+
     return API.script;
 });
+
+
+
+
+
+
+
+
+
+
+
 
